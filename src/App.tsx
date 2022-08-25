e42f150b26d47de8d1678cb508528bc8d11878e6
Chaithra <chaithrapai@gmail.com>
2022-08-25 18:12:10 +0530

    Adding files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LoggedIn } from './components/state/LoggedIn';
+import { User } from './components/state/User';
+import { Counter } from './components/state/Counter';
+import { ThemeContextProvider } from './components/context/ThemeContext';
+import { Box } from './components/context/Box';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>useState Hook</h2>
+      <LoggedIn />
+      <h2>useState Hook 2</h2>
+      <User />
+      <h2>useReducer Hook</h2>
+      <Counter />
+      <h2>useContext Hook</h2>
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
